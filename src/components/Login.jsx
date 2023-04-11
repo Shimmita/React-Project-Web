c12b8a0b94c46f4c3a3325377d297fadd1264ddf
shimmita <douglasshimita3@gmail.com>
2023-04-11 11:22:11 +0300

    initial commit

---
+++ src/components/Login.jsx
@@ -0,0 +1,21 @@
+import classes from "./Login.module.css"
+
+function Login()
+{
+    return <>
+    <div className={classes.container}>
+        <h3 className={classes.headLogin}>USER LOGIN</h3>
+        <form action="" method="post" className={classes.LoginForm}>
+           <div>
+           <input type="text" className={classes.username} placeholder="username" />
+           </div>
+           <div>
+           <input type="text" classes={classes.pass} />
+           </div>
+            <input type="submit" value="Login" classes={classes.btnLogin} />
+        </form>
+    </div>
+    </>
+}
+
+export default Login;
